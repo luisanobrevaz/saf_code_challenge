965ab5327e89cc965f227e8b81ccf8d8b9410fe8
Luisa Vaz <luisanobrevaz@gmail.com>
2024-08-24 16:40:10 -0300

    fix linting

--- run.rb
+++ run.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require './receipt'
 
 all_examples_paths = ['challenge_examples/input1.txt',
-                     'challenge_examples/input2.txt',
-                     'challenge_examples/input3.txt']
+                      'challenge_examples/input2.txt',
+                      'challenge_examples/input3.txt']
 
 file_paths = ARGV[0] ? [ARGV[0]] : all_examples_paths
 
