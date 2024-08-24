2b7ff40977266944f5de57e5fb65390dd6cb6018
Luisa Vaz <luisanobrevaz@gmail.com>
2024-08-24 15:48:07 -0300

    add receipt class and specs

---
+++ receipt.rb
@@ -0,0 +1,33 @@
+class Receipt
+  def initialize(file_path)
+    parsed_items = File.readlines(file_path).map(&:chomp)
+
+    @products = parsed_items.map { |item| Product.new(item) }
+  end
+
+  def print
+    puts receipt_output
+  end
+
+  attr_accessor :products
+
+  private
+
+  def receipt_output
+    lines = products.map do |p|
+      "#{p.quantity} #{p.name}: #{format('%.2f', p.total_price)}"
+    end
+    lines << "Sales Taxes: #{format('%.2f', sales_taxes_sum)}"
+    lines << "Total: #{format('%.2f', total_price_sum)}"
+
+    lines.join("\n")
+  end
+
+  def sales_taxes_sum
+    products.sum(&:total_tax)
+  end
+
+  def total_price_sum
+    products.sum(&:total_price)
+  end
+end
---
+++ spec/receipt_spec.rb
@@ -0,0 +1,28 @@
+require './tax_calculator_service'
+require './receipt'
+require './product'
+
+RSpec.describe Receipt do
+  let(:receipt) { described_class.new(file_path) }
+  let(:file_path) { 'spec/support/fixtures/items_list_example.txt' }
+
+  describe 'initialize' do
+    it 'sets the products' do
+      expect(receipt.products.count).to eq(9)
+    end
+  end
+
+  describe 'print' do
+    let(:expected_print) do
+      "2 book: 24.98\n1 music CD: 16.49\n1 chocolate bar: 0.85\n1 imported " \
+      "box of chocolates: 10.50\n1 imported bottle of perfume: 54.65\n1 " \
+      "imported bottle of perfume: 32.19\n1 bottle of perfume: 20.89\n1 " \
+      "packet of headache pills: 9.75\n3 imported box of chocolates: 35.55\n" \
+      "Sales Taxes: 17.05\nTotal: 205.85\n"
+    end
+
+    it 'prints the receipt' do
+      expect { receipt.print }.to output(expected_print).to_stdout
+    end
+  end
+end
