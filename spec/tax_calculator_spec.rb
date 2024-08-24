11ede57a20273330a2ef1e2ee1dee098364d128a
Luisa Vaz <luisanobrevaz@gmail.com>
2024-08-24 14:59:22 -0300

    improve tax calculator service

--- spec/tax_calculator_spec.rb
+++ spec/tax_calculator_spec.rb
@@ -6,33 +6,49 @@ RSpec.describe TaxCalculatorService do
   let(:call) { described_class.new(product).call }
 
   describe 'init' do
-    let(:product) { Product.new('1 bottle of perfume at 18.99') }
+    let(:product) { Product.new('1 bottle of perfume at 10.00') }
 
     it 'returns the tax' do
-      expect(call).to eq(1.90)
+      expect(call).to eq(1.00)
     end
 
     context 'when product is a book' do
-      let(:product) { Product.new('1 book at 12.49') }
+      let(:product) { Product.new('1 book at 10.00') }
 
-      it 'returns the tax' do
+      it 'returns 0 tax' do
         expect(call).to eq(0)
+      end
+
+      context 'when product is a imported' do
+        let(:product) { Product.new('1 imported book at 10.00') }
+
+        it 'returns the tax' do
+          expect(call).to eq(0.50)
+        end
       end
     end
 
     context 'when product is food' do
-      let(:product) { Product.new('1 chocolate bar at 0.85') }
+      let(:product) { Product.new('1 chocolate bar at 10.00') }
 
-      it 'returns the tax' do
+      it 'returns 0 tax' do
         expect(call).to eq(0)
       end
     end
 
     context 'when product is a medical supply' do
-      let(:product) { Product.new('1 packet of headache pills at 9.75') }
+      let(:product) { Product.new('1 packet of headache pills at 10.00') }
 
-      it 'returns the tax' do
+      it 'returns 0 tax' do
         expect(call).to eq(0)
+      end
+    end
+
+    context 'when product is a imported' do
+      let(:product) { Product.new('1 imported bottle of perfume at 10.00') }
+
+      it 'sums the taxes' do
+        expect(call).to eq(1.50)
       end
     end
   end
--- tax_calculator_service.rb
+++ tax_calculator_service.rb
@@ -13,15 +13,19 @@ class TaxCalculatorService
   private
 
   def round_up_to_nearest_05(number)
-    (number * 20).ceil / 20.0
+    (number.round(2) * 20).ceil / 20.0
   end
 
   def total_tax_rate
-    basic_sales_tax# + import_duty
+    basic_sales_tax + import_duty
   end
 
   def basic_sales_tax
     free_basic_sales_tax_categories = %w[food medical book]
     free_basic_sales_tax_categories.include?(product.category) ? 0 : 0.1
+  end
+
+  def import_duty
+    product.imported ? 0.05 : 0
   end
 end
